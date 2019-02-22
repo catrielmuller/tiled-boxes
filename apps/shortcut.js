98b51ec817cd2f610502a75ce18f9f56a473a2e7
Catriel Müller <catrielmuller@gmail.com>
2019-02-22 17:08:47 -0300

    Speedup StartUp Main

--- apps/shortcut.js
+++ apps/shortcut.js
@@ -1,11 +1,34 @@
-const BaseApp = require('../../lib/tiled-boxes/base/app');
+const calpha = require('color-alpha');
+const robot = require('robot-js');
+const BaseApp = require('../lib/tiled-boxes/base/app');
 
-module.exports = class Base extends BaseApp {
+module.exports = class Shortcut extends BaseApp {
+
+  constructor(props) {
+    super(props);
+    this.name = 'Shortcut';
+
+    this.color = 'green';
+    if (props.color) {
+      this.color = props.color;
+    }
+    this.icon = calpha(this.color, 1);
+    this.keyboard = robot.Keyboard();
+
+    this.comand = false;
+    if (props.keys) {
+      this.keys = props.keys;
+    }
+  }
+
   async setup() {
     return true;
   }
 
   async launch() {
+    if (this.keys) {
+      this.keyboard.click(this.keys);
+    }
     return false;
   }
 
